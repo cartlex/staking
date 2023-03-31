4b4b4dce914730dabb481f1b466fe9e81f3e768f
cartlex <ap26booking@gmail.com>
2023-03-31 22:21:55 +1000

    clean readme

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,17 +1,15 @@
 import eth from "./eth.svg";
 import polygonMatic from "./polygonMatic.svg";
 import optimism from "./optimism.svg";
-import science from "./science.svg";
 import github from "./github.svg";
-import discord from "./discord.svg";
 import telegram from "./telegram.svg";
+import discord from "./discord.svg"
 
 export {
     eth,
     polygonMatic,
     optimism,
-    science,
-    github,
     discord,
+    github,
     telegram
 };
